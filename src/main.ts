48841f9f8f00fc108723857a64c4ff33ccd3911e
Pacoogle <pasquale.ragozzino89@gmail.com>
2020-04-28 18:57:34 +0200

    Add firebase package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYF8jqTzSE15698wNQKVqwpSFkMBXAoT8",
+  authDomain: "tagee-181b1.firebaseapp.com",
+  databaseURL: "https://tagee-181b1.firebaseio.com",
+  projectId: "tagee-181b1",
+  storageBucket: "tagee-181b1.appspot.com",
+  messagingSenderId: "308347881338",
+  appId: "1:308347881338:web:7d6977dffb4866c0b078c3",
+  measurementId: "G-NKBRGL2TC1"
+}
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
