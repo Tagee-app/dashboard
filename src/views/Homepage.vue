8252d493514f0feea6b7c86855cedd83cf2da59c
Pacoogle <pasquale.ragozzino89@gmail.com>
2020-05-02 18:03:51 +0200

    Add homepage view [wip]

---
+++ src/views/Homepage.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <h1> Homepage </h1>
+        <button @click="logout">Logout</button>
+    </v-app>
+</template>
+
+<script>
+    import firebase from 'firebase';
+
+    export default {
+        name: "Homepage",
+        data() {
+            return {}
+        },
+        methods: {
+            logout() {
+                firebase.auth().signOut().then(() => {
+                    this.$router.replace("login");
+                })
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
