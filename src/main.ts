1c7c6aa0ee025221504ba07779e857fbf4447ef4
Pacoogle <pasquale.ragozzino89@gmail.com>
2020-05-02 18:03:33 +0200

    Add protected routes

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,26 @@ import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYF8jqTzSE15698wNQKVqwpSFkMBXAoT8",
-  authDomain: "tagee-181b1.firebaseapp.com",
-  databaseURL: "https://tagee-181b1.firebaseio.com",
-  projectId: "tagee-181b1",
-  storageBucket: "tagee-181b1.appspot.com",
-  messagingSenderId: "308347881338",
-  appId: "1:308347881338:web:7d6977dffb4866c0b078c3",
-  measurementId: "G-NKBRGL2TC1"
+    apiKey: "AIzaSyCYF8jqTzSE15698wNQKVqwpSFkMBXAoT8",
+    authDomain: "tagee-181b1.firebaseapp.com",
+    databaseURL: "https://tagee-181b1.firebaseio.com",
+    projectId: "tagee-181b1",
+    storageBucket: "tagee-181b1.appspot.com",
+    messagingSenderId: "308347881338",
+    appId: "1:308347881338:web:7d6977dffb4866c0b078c3",
+    measurementId: "G-NKBRGL2TC1"
 }
 
-firebase.initializeApp(firebaseConfig);
+let app = '';
 
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp(firebaseConfig);
+firebase.auth().onAuthStateChanged(() => {
+    if (!app) {
+        app = new Vue({
+            router,
+            vuetify,
+            render: h => h(App)
+        }).$mount('#app')
+    }
+});
+
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter, {RouteConfig} from 'vue-router'
 import Login from '../views/Login.vue'
+import Homepage from '../views/Homepage.vue'
+import * as firebase from "firebase";
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
     {
-      path: '*',
-      redirect: '/login'
+        path: '*',
+        redirect: '/login'
     },
     {
         path: '/',
@@ -17,11 +19,28 @@ const routes: Array<RouteConfig> = [
         path: '/login',
         name: 'Login',
         component: Login
+    },
+    {
+        path: '/homepage',
+        name: 'Homepage',
+        component: Homepage,
+        meta: {
+            requiresAuth: true
+        }
     }
 ]
 
 const router = new VueRouter({
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    const currentUser = firebase.auth().currentUser;
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+    if (requiresAuth && !currentUser) next('login')
+    else if (!requiresAuth && currentUser) next('homepage');
+    else next();
+});
 
 export default router
