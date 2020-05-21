e3534f3d70bc47181c2fcd072dc5f37f08eac268
Pasquale Ragozzino <pasquale.ragozzino89@gmail.com>
2020-05-21 10:49:39 +0200

    Add full-calendar

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,10 +1,16 @@
 <template>
     <v-app>
-        <v-container>
+        <v-container style="margin-top: 20px">
             <v-row no-gutters>
                 <v-col cols="2">
-                    <Menu />
+                    <Menu/>
                 </v-col>
+            </v-row>
+        </v-container>
+
+        <v-container>
+            <v-row no-gutters>
+                <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" :locale="locale"/>
             </v-row>
         </v-container>
     </v-app>
@@ -12,18 +18,26 @@
 
 <script>
     import Menu from '../components/Menu';
+    import FullCalendar from '@fullcalendar/vue'
+    import dayGridPlugin from '@fullcalendar/daygrid'
+    import itLocale from '@fullcalendar/core/locales/it';
 
     export default {
         name: "Homepage",
-        components:{
-          Menu
+        components: {
+            Menu,
+            FullCalendar
         },
         data() {
-            return {}
+            return {
+                calendarPlugins: [dayGridPlugin],
+                locale: itLocale
+            }
         },
         methods: {}
     }
 </script>
 <style scoped>
-
+    @import '~@fullcalendar/core/main.css';
+    @import '~@fullcalendar/daygrid/main.css';
 </style>
