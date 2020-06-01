415fe2d61fe7e22b721714b3b2e3ff637987e654
Pacoogle <pasquale.ragozzino89@gmail.com>
2020-06-01 18:23:18 +0200

    Customize Menu styles

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,20 +2,20 @@
     <div>
         <Push :closeOnNavigation="true">
             <button id="homepage" class="button-menu">
-                <i class="fas fa-home"></i><span>Homepage</span>
+                <i class="fas fa-home" style="color: #cd409a"></i><span class="text-menu">Homepage</span>
             </button>
             <button id="report" class="button-menu">
-                <i class="fas fa-chart-bar"></i>
-                <span>Report</span>
+                <i class="fas fa-chart-bar" style="color: #febd13"></i>
+                <span class="text-menu">Report</span>
             </button>
             <button id="settings" class="button-menu">
-                <i class="fas fa-user-cog"></i>
-                <span>Impostazioni</span>
+                <i class="fas fa-user-cog" style="color: #fa8558"></i>
+                <span class="text-menu">Impostazioni</span>
             </button>
-                <button id="logout" @click="logout" class="button-menu bottom-elem">
-                    <i class="fas fa-sign-out-alt"></i>
-                    <span>Logout</span>
-                </button>
+            <button id="logout" @click="logout" class="button-menu bottom-elem">
+                <i class="fas fa-sign-out-alt" style="color: #f25979"></i>
+                <span class="text-menu">Logout</span>
+            </button>
 
         </Push>
     </div>
@@ -48,28 +48,33 @@
 <style>
     body {
         font-family: 'Roboto', 'sans-serif';
+        background-color: aliceblue;
     }
 
     .bm-menu {
-        background-color: #fa8558;
+        background-color: #ffffff;
+        box-shadow: rgb(236, 236, 236) 10px 0px 5px -2px
     }
 
     .bm-cross {
-        background: #ffffff;
+        background: #fa8558;
     }
 
     .bm-burger-bars {
-        background-color: #535353;
+        background-color: #fa8558;
     }
 
-    .button-menu i{
-        color: white;
+    .button-menu i {
         padding-top: 4px;
     }
 
-    .bottom-elem{
+    .bottom-elem {
         bottom: 0px;
         position: absolute;
+    }
+
+    .text-menu {
+        color: #292929 !important;
     }
 
 </style>
