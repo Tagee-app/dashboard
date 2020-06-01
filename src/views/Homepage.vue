fe9b49afd8ccd9c3ca70a368527c20ab20065d0b
Pacoogle <pasquale.ragozzino89@gmail.com>
2020-06-01 18:23:33 +0200

    Add "add new event" feature

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -10,6 +10,7 @@
                             :locale="locale"
                             :showNonCurrentDates="false"
                             @dateClick="handleDateClick"
+                            @eventClick="handleEventClick"
                             :day-render="setNationalHolidays"
                             :events="events"
                     />
@@ -36,6 +37,7 @@
                                            :key="attribute.ref"
                                            :outlined="attribute.outlined"
                                            :color="attribute.color"
+                                           disabled="attribute.disable"
                                            @click="changeButtonState($event, attribute.ref)"
                                     >
                                         <span :style="[!attribute.outlined ? {'color' : 'white'} : {}]">
@@ -66,6 +68,7 @@
                                                :key="attribute.ref"
                                                :outlined="attribute.outlined"
                                                :color="attribute.color"
+                                               :disabled="attribute.disable"
                                                @click="changeButtonState($event, attribute.ref)"
                                         >
                                         <span :style="[!attribute.outlined ? {'color' : 'white'} : {}]">
@@ -111,16 +114,34 @@
                                 </v-chip>
                             </v-row>
 
+                            <div :hidden="showPermissionForm">
+                                <v-row justify="space-around" align="center">
+                                    <v-col style="width: 290px; flex: 0 1 auto;">
+                                        <h2>Dalle</h2>
+                                        <v-time-picker v-model="timePicker.start" :max="timePicker.end" format="24hr"
+                                                       :allowed-minutes="allowedMinutes"></v-time-picker>
+                                    </v-col>
+                                    <v-col style="width: 290px; flex: 0 1 auto;">
+                                        <h2>Alle</h2>
+                                        <v-time-picker v-model="timePicker.end" :min="timePicker.start" format="24hr"
+                                                       :allowed-minutes="allowedMinutes"></v-time-picker>
+                                    </v-col>
+                                </v-row>
+                            </div>
 
                             <small>
-                                Tutte le richieste prima di essere confermate, dovranno essere accettate dal tuo
+                                * Tutte le richieste prima di essere confermate, dovranno essere accettate dal tuo
                                 referente.
                             </small>
                         </v-card-text>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn outlined color="success" @click="dialog = false">Salva</v-btn>
-                            <v-btn outlined color="error" @click="dialog = false">Annulla</v-btn>
+                            <v-btn outlined color="success" @click="saveEvent" :disabled="successButton">
+                                Salva
+                            </v-btn>
+                            <v-btn outlined color="error" @click="dialog = false">
+                                Annulla
+                            </v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-dialog>
@@ -156,6 +177,7 @@
                 sound: true,
                 widgets: false,
                 requestData: "",
+                fromDateRequest: "",
                 outlinedButton: true,
                 buttons: [
                     {
@@ -163,6 +185,7 @@
                             color: "#cd409a",
                             ref: "ferie",
                             outlined: true,
+                            disable: false,
                         }
                     },
                     {
@@ -170,6 +193,7 @@
                             color: "#febd13",
                             ref: "permesso",
                             outlined: true,
+                            disable: false,
                         }
                     },
                     {
@@ -177,6 +201,7 @@
                             color: "#a455bc",
                             ref: "malattia",
                             outlined: true,
+                            disable: false,
                         }
                     },
                     {
@@ -184,6 +209,7 @@
                             color: "#fa8558",
                             ref: "smart working",
                             outlined: true,
+                            disable: false,
                         }
                     },
                     {
@@ -191,6 +217,7 @@
                             color: "#f25979",
                             ref: "congedo matrimoniale",
                             outlined: true,
+                            disable: false,
                         }
                     },
                     {
@@ -198,15 +225,32 @@
                             color: "#cd409a",
                             ref: "maternità",
                             outlined: true,
+                            disable: false,
                         }
                     }
                 ],
-                formType: "",
+                eventType: "",
+                showPermissionForm: true,
                 holidaysCount: 0,
                 permissionsCount: 6,
                 sicknessCount: 2,
                 events: [],
-                nationalHolidays: ['0-1', '0-6', '3-25', '4-1', '5-2', '7-15', '10-1', '11-8', '11-25', '11-26']
+                nationalHolidays: ['0-1', '0-6', '3-25', '4-1', '5-2', '7-15', '10-1', '11-8', '11-25', '11-26'],
+                successButton: true,
+                timePicker: {
+                    start: null,
+                    end: null,
+                }
+            }
+        },
+        watch: {
+            timePicker: {
+                handler: function (val) {
+                    if (val.end !== null) {
+                        this.successButton = false; // enable send button
+                    }
+                },
+                deep: true
             }
         },
         beforeMount() {
@@ -215,7 +259,44 @@
         methods: {
             handleDateClick(arg) {
                 this.requestData = this.formatDateToItalianDate(arg.dateStr);
+                this.fromDateRequest = arg.dateStr;
                 this.dialog = true;
+
+                // check events existing
+                for (let i = 0; this.events.length > i; i++) {
+                    if (this.events[i].start === arg.dateStr) {
+                        this.toggleButtons(this.events[i].type, true);
+                        return;
+                    } else this.toggleButtons(this.events[i].type, false);
+                }
+            },
+            handleEventClick(event) {
+                console.log(event)
+            },
+            saveEvent() {
+                const events = firebase.firestore().collection('events');
+
+                events.add({
+                    fromDate: this.fromDateRequest,
+                    toDate: "",
+                    fromHour: this.timePicker.start,
+                    toHour: this.timePicker.end,
+                    type: this.eventType,
+                    uid: firebase.auth().currentUser.uid,
+                    approved: false
+                }).then((docRef) => {
+                    this.events.push({
+                        id: docRef.id,
+                        title: 'Ferie',
+                        backgroundColor: this.eventType === 'ferie' ? 'rgba(205,64,154,0.20)' : '#fa8558',
+                        borderColor: this.eventType === 'ferie' ? 'rgba(205,64,154,0.20)' : '#fa8558',
+                        textColor: 'black',
+                        start: this.fromDateRequest,
+                        type: this.eventType,
+                    });
+                }).catch(function (error) {
+                    console.error("Error writing document: ", error);
+                });
             },
             setNationalHolidays(dayRenderInfo) {
                 const date = dayRenderInfo.date.getMonth() + "-" + dayRenderInfo.date.getDate()
@@ -230,15 +311,16 @@
 
                     documents.docs.forEach((doc) => {
                         if (doc.data().type === "ferie") {
-                            this.holidaysCount++;
+                            if (doc.data().approved) this.holidaysCount++;
 
                             this.events.push({
                                 id: doc.id,
                                 title: 'Ferie',
-                                backgroundColor: '#cd409a',
-                                borderColor: '#cd409a',
-                                textColor: 'white',
-                                start: doc.data().fromDate
+                                backgroundColor: doc.data().approved ? '#cd409a' : 'rgba(205,64,154,0.20)',
+                                borderColor: doc.data().approved ? '#cd409a' : 'rgba(205,64,154,0.20)',
+                                textColor: doc.data().approved ? 'white' : 'black',
+                                start: doc.data().fromDate,
+                                type: doc.data().type,
                             })
                         }
                     });
@@ -267,12 +349,29 @@
 
                     if (button !== undefined) {
                         button.outlined = !button.outlined;
-                        if (this.camelize(elem) === 'permesso') {
-                            this.formType = 'permesso';
-                        }
+                        this.eventType = this.camelize(elem);
                     } else {
                         const otherKey = Object.keys(this.buttons[i])[0];
                         this.buttons[i][otherKey].outlined = true;
+                    }
+                }
+
+                if (this.eventType === 'permesso') {
+                    this.successButton = true; // disable send button
+                    this.showPermissionForm = false;
+                } else {
+                    this.successButton = false; // enable send button
+                    this.showPermissionForm = true;
+                }
+            },
+            toggleButtons(eventType, state) {
+                const buttons = this.buttons;
+                for (let i = 0; buttons.length > i; i++) {
+                    if (buttons[i][this.camelize(eventType)] !== undefined) {
+                        buttons[i][this.camelize(eventType)].disable = state;
+                    } else {
+                        const otherKey = Object.keys(this.buttons[i])[0];
+                        buttons[i][otherKey].disable = state;
                     }
                 }
             },
@@ -282,7 +381,8 @@
                     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
                 })(navigator.userAgent || navigator.vendor || window.opera);
                 return check;
-            }
+            },
+            allowedMinutes: v => v % 5 === 0
         }
     }
 </script>
